d51793a473deb24f18f5b32aa7984e9893069571
ben <ben.gabay38@gmail.com>
2019-08-03 23:09:47 +0300

    added gray value to the colors xml

--- app/src/main/java/com/example/user/amd/interfaces/lambdaFunctions/loginButtonHandler.java
+++
@@ -1,7 +0,0 @@
-package com.example.user.amd.interfaces.lambdaFunctions;
-
-import android.widget.Button;
-
-public interface loginButtonHandler {
-    void handle(Button loginButton, String username, String password);
-}
