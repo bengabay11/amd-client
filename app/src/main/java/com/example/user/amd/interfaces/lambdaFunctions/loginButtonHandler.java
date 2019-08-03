12d8bdcc0af9cb5acde2ee82ed0db5992162bb29
ben <ben.gabay38@gmail.com>
2019-08-03 16:39:41 +0300

    added login button hadnler interface

---
+++ app/src/main/java/com/example/user/amd/interfaces/lambdaFunctions/loginButtonHandler.java
@@ -0,0 +1,7 @@
+package com.example.user.amd.interfaces.lambdaFunctions;
+
+import android.widget.Button;
+
+public interface loginButtonHandler {
+    void handle(Button loginButton, String username, String password);
+}
