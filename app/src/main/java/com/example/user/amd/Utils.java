a221a3ad2744689643663604ff4d30638cbbd5ee
ben <ben.gabay38@gmail.com>
2019-11-23 22:41:37 +0200

    init the socket task class with server data handlers

--- app/src/main/java/com/example/user/amd/Utils.java
+++ app/src/main/java/com/example/user/amd/Utils.java
@@ -1,22 +1,35 @@
 package com.example.user.amd;
 
 import android.app.Activity;
-import android.content.DialogInterface;
 import android.graphics.Point;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.Display;
 
-import com.example.user.amd.activities.ConnectedActivity;
-import com.example.user.amd.activities.MainActivity;
 import com.example.user.amd.enums.ServerDataType;
 import com.example.user.amd.interfaces.IServerDataHandler;
+import com.example.user.amd.server_data_handlers.EmailChangedHandler;
+import com.example.user.amd.server_data_handlers.ChangePasswordNeededHandler;
+import com.example.user.amd.server_data_handlers.EmailSentHandler;
+import com.example.user.amd.server_data_handlers.FailedSendEmailHandler;
+import com.example.user.amd.server_data_handlers.IncorrectPasswordHandler;
+import com.example.user.amd.server_data_handlers.IncorrectUsernameHandler;
+import com.example.user.amd.server_data_handlers.InvalidEmailHandler;
+import com.example.user.amd.server_data_handlers.LoadingCompleteHandler;
+import com.example.user.amd.server_data_handlers.LoginCompletedHandler;
+import com.example.user.amd.server_data_handlers.LogoutCompletedHandler;
+import com.example.user.amd.server_data_handlers.PasswordAcceptedHandler;
+import com.example.user.amd.server_data_handlers.PasswordChangedHandler;
+import com.example.user.amd.server_data_handlers.UsernameAcceptedHandler;
+import com.example.user.amd.server_data_handlers.UsernameChangedHandler;
+import com.example.user.amd.server_data_handlers.UsernameDoesntExistHandler;
+import com.example.user.amd.server_data_handlers.UsernameExistHandler;
 import com.example.user.amd.tasks.SocketTask;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 
@@ -60,8 +73,25 @@ public class Utils {
 
     public static SocketTask runSocketTask(Activity currentActivity)
     {
-        // TODO: add init for the server data handlers
-        Dictionary<ServerDataType, IServerDataHandler> serverDataHandlers = null;
+        Map<ServerDataType, IServerDataHandler> serverDataHandlers =
+                new HashMap<ServerDataType, IServerDataHandler>(){{
+            put(ServerDataType.ChangePasswordNeeded, new ChangePasswordNeededHandler());
+            put(ServerDataType.EmailChanged, new EmailChangedHandler());
+            put(ServerDataType.EmailSent, new EmailSentHandler());
+            put(ServerDataType.FailedSendingEmail, new FailedSendEmailHandler());
+            put(ServerDataType.IncorrectPassword, new IncorrectPasswordHandler());
+            put(ServerDataType.IncorrectUsername, new IncorrectUsernameHandler());
+            put(ServerDataType.InvalidEmail, new InvalidEmailHandler());
+            put(ServerDataType.LoadingComplete, new LoadingCompleteHandler());
+            put(ServerDataType.LoginCompleted, new LoginCompletedHandler());
+            put(ServerDataType.LogoutCompleted, new LogoutCompletedHandler());
+            put(ServerDataType.PasswordAccepted, new PasswordAcceptedHandler());
+            put(ServerDataType.PasswordChanged, new PasswordChangedHandler());
+            put(ServerDataType.UsernameAccepted, new UsernameAcceptedHandler());
+            put(ServerDataType.UsernameChanged, new UsernameChangedHandler());
+            put(ServerDataType.UsernameDoesntExist, new UsernameDoesntExistHandler());
+            put(ServerDataType.UsernameExist, new UsernameExistHandler());
+        }};
         SocketTask socketTask = new SocketTask(currentActivity, serverDataHandlers);
         socketTask.execute();
         return socketTask;
